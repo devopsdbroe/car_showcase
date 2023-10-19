3cf3a9c69efdeb57d6d9baf506514051621db538
devopsdbroe <dbroe1995@gmail.com>
2023-10-18 21:52:08 -0400

    Implemented initial Modal

---
+++ utils/index.ts
@@ -0,0 +1,27 @@
+export async function fetchCars() {
+    const headers = {
+        'X-RapidAPI-Key': '6f9068e73emsh28483ee33f924d5p114cfajsne7da967785e1',
+		'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
+    }
+
+    const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla', {headers: headers,});
+
+    const result = await response.json();
+
+    return result;
+}
+
+export const calculateCarRent = (city_mpg: number, year: number) => {
+    const basePricePerDay = 50; // Base rental price per day in dollars
+    const mileageFactor = 0.1; // Additional rate per mile driven
+    const ageFactor = 0.05; // Additional rate per year of vehicle age
+  
+    // Calculate additional rate based on mileage and age
+    const mileageRate = city_mpg * mileageFactor;
+    const ageRate = (new Date().getFullYear() - year) * ageFactor;
+  
+    // Calculate total rental rate per day
+    const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
+  
+    return rentalRatePerDay.toFixed(0);
+  };
